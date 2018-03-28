34e4e34287a18eda3f38318dfbb1f559e27c81fe
Radek Bartyzal <radek.bartyzal@recombee.com>
2018-03-28 17:42:20 +0200

    Fixed A generation error.

--- linUCB.py
+++ linUCB.py
@@ -18,7 +18,7 @@ class LinUCB:
         # More efficient way to create array of identity matrices of length num_items
         print("Initializing matrix A of shape {} which will require {}MB of memory.".format(self.A.shape,
                                                                                             8 * self.A.size / 1e6))
-        #self.A = np.tile(np.identity(self.d, dtype=self.A.dtype), (dataset.num_items, 1))
+        self.A = np.repeat(np.identity(self.d, dtype=self.A.dtype)[np.newaxis, :, :], dataset.num_items, axis=0)
         print("\nLinUCB successfully initialized.")
 
     def choose_arm(self, t):
